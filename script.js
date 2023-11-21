37a7f60a38f7108db76ba6a64e6bea3ce7e9d9f7
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2023-11-21 20:16:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const currentNum = document.getElementById('counter');
+const incrementButton = document.getElementById('incrementBtn');
+
+let count = 0;
+
+function buttonClick(){
+
+	alert(count);
+	count++;
+	
+	
+	document.getElementById('counter').textContent = count;
+	
+}
